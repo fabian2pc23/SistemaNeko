da68e943e23cf319dc198f31b1193bd841723b1b
fabian2pc23 <150277435+fabian2pc23@users.noreply.github.com>
2025-10-16 23:49:48 -0500

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST", "127.0.0.1");
+define("DB_HOST", "localhost");
 define("DB_NAME", "bd_ferreteria");
-define("DB_USERNAME", "root"); // O admin si ya diste permisos
-define("DB_PASSWORD", "");
+define("DB_USERNAME", "admin"); // O admin si ya diste permisos
+define("DB_PASSWORD", "76960068");
 define("DB_CHARSET", "utf8mb4");
 define("DB_COLLATION", "utf8mb4_general_ci");
