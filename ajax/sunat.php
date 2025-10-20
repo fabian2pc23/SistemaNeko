8c06b5de76ba031bc9de62c1e3088520bc7fa776
fabian2pc23 <150277435+fabian2pc23@users.noreply.github.com>
2025-10-20 01:04:40 -0500

    Update sunat.php

--- ajax/sunat.php
+++ ajax/sunat.php
@@ -1,4 +1,8 @@
 <?php
+// AGREGAR ESTAS DOS LÍNEAS TEMPORALMENTE
+ini_set('display_errors', '1'); 
+error_reporting(E_ALL); 
+// FIN AGREGADO
 // ajax/sunat.php
 declare(strict_types=1);
 header('Content-Type: application/json; charset=utf-8');
