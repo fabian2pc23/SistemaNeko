9858b48b4d5a8f28b7ba12351f9c8bf8ed642b00
fabian2pc23 <150277435+fabian2pc23@users.noreply.github.com>
2025-10-21 16:07:32 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,12 @@
 <?php
 
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', '/tmp/php-error.log');
+error_reporting(E_ALL);
+
+echo "✅ Debug activo<br>";
 
 // src/register.php
 declare(strict_types=1);
